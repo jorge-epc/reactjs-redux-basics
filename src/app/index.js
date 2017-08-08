990dd5916a00ed2cc5d4489cce56861ce0ba4940
Jorge Padilla <jorge.esteban.padilla@gmail.com>
2017-08-08 16:07:36 -0500

    Test state, inmutability, multiple reducers and middleware in Redux

--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import { render } from 'react-dom';
 
 import { User } from './components/User';
@@ -28,4 +28,86 @@ class App extends React.Component {
   }
 }
 
-render(<App />, window.document.getElementById('app'));
+render(<App />, window.document.getElementById('app'));*/
+
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+
+const mathReducer = (state = {
+  result: 1,
+  lastValues: [],
+}, action) => {
+  switch (action.type) {
+    case 'ADD':
+      state = {
+        ...state,
+        result: state.result + action.payload,
+        lastValues: [...state.lastValues, action.payload],
+      };
+      break;
+    case 'SUBSTRACT':
+      state = {
+        ...state,
+        result: state.result - action.payload,
+        lastValues: [...state.lastValues, action.payload],
+      };
+      break;
+  }
+  return state;
+};
+
+const userReducer = (state = {
+  name: 'Max',
+  age: 27,
+}, action) => {
+  switch (action.type) {
+    case 'SET_NAME':
+      state = {
+        ...state,
+        name: action.payload,
+      };
+      break;
+    case 'SET_AGE':
+      state = {
+        ...state,
+        age: action.payload,
+      };
+      break;
+  }
+  return state;
+};
+
+const myLogger = (store) => (next) => (action) => {
+  console.log('Logged action: ', action);
+  next(action);
+};
+
+const store = createStore(
+  combineReducers({ mathReducer, userReducer }),
+  {},
+  applyMiddleware(logger),
+);
+
+store.subscribe(() => {
+  // console.log('Store update', store.getState());
+});
+
+store.dispatch({
+  type: 'ADD',
+  payload: 100,
+});
+
+store.dispatch({
+  type: 'ADD',
+  payload: 5,
+});
+
+store.dispatch({
+  type: 'SUBSTRACT',
+  payload: 50,
+});
+
+store.dispatch({
+  type: 'SET_AGE',
+  payload: 30,
+});
